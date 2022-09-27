c9917a79e998174d7db60c7bf199a4fbcbc71a2c
Rémy Cspl <remy.jabbo@gmail.com>
2022-09-27 16:30:41 +0200

    Skeleton done

--- Assets/Scritps/ChoiceManager.cs
+++ Assets/Scritps/ChoiceManager.cs
@@ -11,14 +11,53 @@ public class ChoiceManager : MonoBehaviour
     public Image CharacterImage;
     public TextMeshProUGUI TitleText;
     public TextMeshProUGUI DialogText;
+    public Button ButtonNext;
+    public Button NextChoice1;
+    public Button NextChoice2;
 
 
     void Start()
     {
-        BackgroundImage.sprite = CurrentChoice.BackgroundSprite;
-        CharacterImage.sprite = CurrentChoice.CharacterSprite;
-        TitleText.text = CurrentChoice.TitleText;
-        DialogText.text = CurrentChoice.DialogText;
+        UpdateChoice( CurrentChoice );
+    }
+
+    void UpdateChoice ( Choice choice )
+    {
+        if(choice.NextChoice1 == null)
+        {
+            ButtonNext.interactable = false;
+        }
+
+        if(choice.HasAChoice == false)
+        {
+            NextChoice1.gameObject.SetActive(false);
+            NextChoice2.gameObject.SetActive(false);
+            ButtonNext.gameObject.SetActive(true);
+        }
+
+        else
+        {
+            NextChoice1.gameObject.SetActive(true);
+            NextChoice2.gameObject.SetActive(true);
+            ButtonNext.gameObject.SetActive(false);
+        }
+
+        BackgroundImage.sprite = choice.BackgroundSprite;
+        CharacterImage.sprite = choice.CharacterSprite;
+        TitleText.text = choice.TitleText;
+        DialogText.text = choice.DialogText;
+    }
+
+    public void OnClickNextDialog1()
+    {
+        CurrentChoice = CurrentChoice.NextChoice1;
+        UpdateChoice ( CurrentChoice );
+    }
+
+    public void OnClickNextDialog2()
+    {
+        CurrentChoice = CurrentChoice.NextChoice2;
+        UpdateChoice ( CurrentChoice );
     }
 
 
